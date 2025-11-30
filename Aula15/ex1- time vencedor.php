73632a685f8d27275b6110ace9d807ee934cc911
Eduarda Oliveira <eduardadeoliveira1@hotmail.com>
2025-11-30 09:23:42 -0300

    atualizando time vencedor - codigo correto

--- Aula15/ex1- time vencedor.php
+++ Aula15/ex1- time vencedor.php
@@ -1,96 +1,74 @@
 <?php
 
-class Endereco {
-    private $rua;
-    private $bairro;
-    private $cidade;
-
-    public function __construct($rua, $bairro, $cidade) {
-        $this->rua = $rua;
-        $this->bairro = $bairro;
-        $this->cidade = $cidade;
-    }
-
-    public function getRua() {
-        return $this->rua;
-    }
-
-    public function getBairro() {
-        return $this->bairro;
-    }
-
-    public function getCidade() {
-        return $this->cidade;
-    }
-
-    public function toArray() {
-        return [
-            "rua" => $this->rua,
-            "bairro" => $this->bairro,
-            "cidade" => $this->cidade
-        ];
-    }
-}
-
-class Pessoa {
+class Time {
     private $nome;
-    private $cpf;
-    private $email;
-    private $endereco;        
-    private $telefone;
-    private $dataNascimento;
-    private $estadoCivil;
-    private $genero;
+    private $quantidadeJogadores;
 
-    public function __construct(
-        $nome,
-        $cpf,
-        $email,
-        Endereco $endereco,
-        $telefone,
-        $dataNascimento,
-        $estadoCivil,
-        $genero
-    ) {
+    public function __construct($nome, $quantidadeJogadores) {
         $this->nome = $nome;
-        $this->cpf = $cpf;
-        $this->email = $email;
-        $this->endereco = $endereco;
-        $this->telefone = $telefone;
-        $this->dataNascimento = $dataNascimento;
-        $this->estadoCivil = $estadoCivil;
-        $this->genero = $genero;
+        $this->quantidadeJogadores = $quantidadeJogadores;
     }
-
 
     public function getNome() {
         return $this->nome;
     }
+}
 
-    public function setNome($novoNome) {
-        $this->nome = $novoNome;
+class Jogador {
+    private $nome;
+    private $numeroCamisa;
+
+    public function __construct($nome, $numeroCamisa) {
+        $this->nome = $nome;
+        $this->numeroCamisa = $numeroCamisa;
     }
 
+    public function getNome() {
+        return $this->nome;
+    }
+}
 
-    public function getCpf() { return $this->cpf; }
-    public function getEmail() { return $this->email; }
-    public function getEndereco() { return $this->endereco; }
-    public function getTelefone() { return $this->telefone; }
-    public function getDataNascimento() { return $this->dataNascimento; }
-    public function getEstadoCivil() { return $this->estadoCivil; }
-    public function getGenero() { return $this->genero; }
+class Jogo {
+    private $timeA;
+    private $timeB;
+    private $golsA;
+    private $golsB;
 
-    public function toArray() {
-        return [
-            "nome" => $this->nome,
-            "cpf" => $this->cpf,
-            "email" => $this->email,
-            "telefone" => $this->telefone,
-            "dataNascimento" => $this->dataNascimento,
-            "estadoCivil" => $this->estadoCivil,
-            "genero" => $this->genero,
-            "endereco" => $this->endereco->toArray()
-        ];
+    public function __construct(Time $timeA, Time $timeB, $golsA, $golsB) {
+        $this->timeA = $timeA;
+        $this->timeB = $timeB;
+        $this->golsA = $golsA;
+        $this->golsB = $golsB;
+    }
+
+    public function descobrirVencedor() {
+        if ($this->golsA > $this->golsB) {
+            return $this->timeA;
+        } elseif ($this->golsB > $this->golsA) {
+            return $this->timeB;
+        } else {
+            return null; // Empate
+        }
+    }
+}
+
+class Partida {
+    private $jogo;
+    private $jogadorDestaque;
+
+    public function __construct(Jogo $jogo, Jogador $jogadorDestaque) {
+        $this->jogo = $jogo;
+        $this->jogadorDestaque = $jogadorDestaque;
+    }
+
+    public function resultadoFinal() {
+        $vencedor = $this->jogo->descobrirVencedor();
+
+        if ($vencedor === null) {
+            return "A partida terminou empatada.";
+        }
+
+        return "O time vencedor foi: " . $vencedor->getNome();
     }
 }
 
